c7c60eb100959f9af270368dce540f11c5ab073d
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-09-09 11:35:52 -0400

    Adds targetting js and animation of modal

--- app/js/modal.js
+++ app/js/modal.js
@@ -2,9 +2,46 @@ var portfolioItems = document.querySelector('.portfolio__items');
 
 portfolioItems.addEventListener('click', function(e){
     e.preventDefault();
-    portfolioItem = e.target
-    console.log(portfolioItem)
-    // console.log(portfolioItem.closest('.portfolio__item-link'))
+    portfolioItem = e.target.closest('.portfolio__item');
+    figCaption = portfolioItem.querySelector('.portfolio__caption');
+    modalLink = figCaption.querySelector('.portfolio__item-link');
+    
+    if (! modalLink ) return
+
+    var modal = portfolioItem.querySelector('.portfolio__modal');
+    var closeButton = modal.querySelector('.menu-close');
+
+    function openModal(){
+        modal.classList.add('is-open');
+        modal.animate([
+            // keyframes
+            { transform: 'translatey(-300px)' }, 
+            { transform: 'translatey(0px)' }
+          ], { 
+            // timing options
+            duration: 500,
+            iterations: 1
+          });
+    }
+
+    openModal();
+
+    function modalClose(){
+        modal.classList.remove('is-open');
+        // modal.removeEventListener('animationend', modalClose())
+    }
+
+    
+    
+
+
+
+
+
+
+    // console.log(portfolioItem.querySelector('.portfolio__item--link'))
+    // closestFigCaption = portfolioItem.closest("figcaption");
+    // console.log(closestFigCaption.closest('.portfolio__item--link'));
 
     // modalToggle = e.target.lastElementChild;
     // console.log(modalToggle);
