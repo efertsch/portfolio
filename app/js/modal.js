314302002a38acada30ff484271c0ee8e2a7a7d4
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-09 17:57:17 -0400

    Tests use of button instead of a link for modal

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,53 +1,31 @@
 var portfolioItems = document.querySelector('.portfolio__items');
 
 portfolioItems.addEventListener('click', function(e){
-    e.preventDefault();
-    portfolioItem = e.target.closest('.portfolio__item');
-    figCaption = portfolioItem.querySelector('.portfolio__caption');
-    modalLink = figCaption.querySelector('.portfolio__item-link');
-    
-    if (! modalLink ) return
 
-    var modal = portfolioItem.querySelector('.portfolio__modal');
+    var modalToggle = e.target.closest('.portfolio__item-link');
+    
+    if (! modalToggle ) return
+
+    var modal = modalToggle.parentNode.nextElementSibling;
     var closeButton = modal.querySelector('.menu-close');
+    
 
     function openModal(){
         modal.classList.add('is-open');
-        modal.animate([
-            // keyframes
-            { transform: 'translatey(-300px)' }, 
-            { transform: 'translatey(0px)' }
-          ], { 
-            // timing options
-            duration: 500,
-            iterations: 1
-          });
+        modal.style.animation = 'modalIn 1000ms forwards'
     }
+
+    function closeModal(){
+        modal.removeEventListener('click', closeModal)
+    }
+
+    closeButton.addEventListener('click', function() {
+        modal.classList.remove('is-open')
+        modal.style.animation = 'modalOut 500ms forwards'
+      })
 
     openModal();
 
-    function modalClose(){
-        modal.classList.remove('is-open');
-        // modal.removeEventListener('animationend', modalClose())
-    }
-
-    
-    
-
-
-
-
-
-
-    // console.log(portfolioItem.querySelector('.portfolio__item--link'))
-    // closestFigCaption = portfolioItem.closest("figcaption");
-    // console.log(closestFigCaption.closest('.portfolio__item--link'));
-
-    // modalToggle = e.target.lastElementChild;
-    // console.log(modalToggle);
-    // if (! modalToggle) return
-    
-    // modal = modalToggle.parentNode.nextElementSibling;
     
 });
 
