a38c477f551096476ea0cf89603c80d1e6fb77ff
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-09 22:43:48 -0400

    Finishes scripts for modals

--- app/js/modal.js
+++ app/js/modal.js
@@ -8,11 +8,13 @@ portfolioItems.addEventListener('click', function(e){
 
     var modal = modalToggle.parentNode.nextElementSibling;
     var closeButton = modal.querySelector('.menu-close');
+    var backdrop = document.querySelector('#modal-backdrop');
+
     
 
-    function openModal(){
+   function openModal(){
         modal.classList.add('is-open');
-        modal.style.animation = 'modalIn 1000ms forwards'
+        backdrop.style.display = 'block';
     }
 
     function closeModal(){
@@ -21,16 +23,8 @@ portfolioItems.addEventListener('click', function(e){
 
     closeButton.addEventListener('click', function() {
         modal.classList.remove('is-open')
-        modal.style.animation = 'modalOut 500ms forwards'
+        backdrop.style.display = 'none';
       })
-
-    openModal();
-
-    
+      
+    openModal(); 
 });
-
-//When a user clicks on the screen 
-    //Get the closest portfolio item 
-    //Grab the modal that is a sibling of that portfolio item and open it (animate)
-    //Close the modal when the close icon is clicked (animate)
-    //close the modal if the user hits the escape key 
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,61 +1,33 @@
 var portfolioItems = document.querySelector('.portfolio__items');
 
 portfolioItems.addEventListener('click', function(e){
-    e.preventDefault();
-    portfolioItem = e.target.closest('.portfolio__item');
-    figCaption = portfolioItem.querySelector('.portfolio__caption');
-    modalLink = figCaption.querySelector('.portfolio__item-link');
-    
-    if (! modalLink ) return
 
-    var modal = portfolioItem.querySelector('.portfolio__modal');
+    var modalToggle = e.target.closest('.portfolio__item-link');
+    
+    if (! modalToggle ) return
+
+    var modal = modalToggle.parentNode.nextElementSibling;
     var closeButton = modal.querySelector('.menu-close');
+    var backdrop = document.querySelector('#modal-backdrop');
 
-    function openModal(){
+    
+
+   function openModal(){
         modal.classList.add('is-open');
-        modal.animate([
-            // keyframes
-            { transform: 'translatey(-300px)' }, 
-            { transform: 'translatey(0px)' }
-          ], { 
-            // timing options
-            duration: 500,
-            iterations: 1
-          });
+        backdrop.style.display = 'block';
     }
 
-    openModal();
-
-    function modalClose(){
-        modal.classList.remove('is-open');
-        // modal.removeEventListener('animationend', modalClose())
+    function closeModal(){
+        modal.removeEventListener('click', closeModal)
     }
 
-    
-    
-
-
-
-
-
-
-    // console.log(portfolioItem.querySelector('.portfolio__item--link'))
-    // closestFigCaption = portfolioItem.closest("figcaption");
-    // console.log(closestFigCaption.closest('.portfolio__item--link'));
-
-    // modalToggle = e.target.lastElementChild;
-    // console.log(modalToggle);
-    // if (! modalToggle) return
-    
-    // modal = modalToggle.parentNode.nextElementSibling;
-    
+    closeButton.addEventListener('click', function() {
+        modal.classList.remove('is-open')
+        backdrop.style.display = 'none';
+      })
+      
+    openModal(); 
 });
-
-//When a user clicks on the screen 
-    //Get the closest portfolio item 
-    //Grab the modal that is a sibling of that portfolio item and open it (animate)
-    //Close the modal when the close icon is clicked (animate)
-    //close the modal if the user hits the escape key 
 
 
 var openMenuButton = document.querySelector('.menu-button');
