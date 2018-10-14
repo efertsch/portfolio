a2318a569852ba3e8fadf22434d6d53e61d0a29b
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-13 20:33:40 -0400

    Serves from dist files

--- dist/js/main.js
+++ dist/js/main.js
@@ -76,11 +76,3 @@ function closeNavOnScroll(links, className){
 
 toggleNav();
 closeNavOnScroll(menuLinks, "open-nav");
-var rellax = new Rellax('.rellax', {
-    speed: -2,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-});
